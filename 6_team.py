b1230b9c1460d76c3ad54fde066cd9ff036d1d45
onlyphantom <samuel@outlook.sg>
2023-04-16 18:52:52 +0700

    add query website with gpt example

---
+++ 6_team.py
@@ -0,0 +1,33 @@
+
+from dotenv import load_dotenv
+from llama_index import GPTChromaIndex, TrafilaturaWebReader
+import chromadb
+
+load_dotenv()
+
+def create_embedding_store(name):
+    chroma_client = chromadb.Client()
+    return chroma_client.create_collection("newspieces")
+
+def query_pages(collection, urls, questions):
+    docs = TrafilaturaWebReader().load_data(urls)
+    index = GPTChromaIndex.from_documents(docs, chroma_collection=collection)
+    for question in questions:
+        print(f"Question: {question} \n")
+        print(f"Answer: {index.query(question)}")
+
+if __name__ == "__main__":
+    url_list = ["https://supertype.ai", "https://supertype.ai/about-us"]
+    questions = [
+        "Who are the members of Supertype.ai", 
+        "What problems are they trying to solve?",
+        "What are the important values at the company?"
+    ]
+
+    collection = create_embedding_store("supertype")
+
+    query_pages(
+        collection,
+        url_list,
+        questions
+    )

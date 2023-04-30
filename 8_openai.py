38b780a35f15dd9d84d6c919b9ee279137bb4d82
onlyphantom <samuel@outlook.sg>
2023-04-30 21:59:18 +0700

    add openai example

---
+++ 8_openai.py
@@ -0,0 +1,29 @@
+import openai
+from dotenv import load_dotenv
+import webbrowser
+
+load_dotenv()
+
+# list all models
+models = openai.Model.list()
+print(models.data[0].root)
+
+# create our completion
+completion = openai.Completion.create(model="ada", prompt="Bill Gates is a")
+print(completion.choices[0].text)
+
+image_gen = openai.Image.create(prompt="Zwei Hunde spielen unter einem Baum, cartoon",
+                                n=2,
+                                size="512x512"
+                            )
+# imgurl1 = image_gen.data[0].url
+# imgurl2 = image_gen.data[1].url
+# webbrowser.open(imgurl)
+for img in image_gen.data:
+    webbrowser.open_new_tab(img.url)
+
+
+# Gwendolyn Brooks Writers' Conference - Keynote Address: Dr. Donda West
+audio = open("audio/donda.mp3", "rb")
+transcript = openai.Audio.transcribe("whisper-1", audio)
+print(transcript)

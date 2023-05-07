e65b8101e0c68fff8ac7b3c398af182a2a7aafeb
onlyphantom <samuel@outlook.sg>
2023-05-07 16:34:51 +0700

    add ai tutor updated code and resources, such as txt files

--- 9_pinecone.py
+++ 9_pinecone.py
@@ -1,54 +1,48 @@
 import os
+from pathlib import Path
 import requests
 from dotenv import load_dotenv
 from bs4 import BeautifulSoup
+
+import pinecone
 from llama_index import (
     SimpleDirectoryReader,
     LLMPredictor,
     ServiceContext,
     GPTVectorStoreIndex,
     QuestionAnswerPrompt,
+    download_loader
 )
-import pinecone
 from llama_index.vector_stores import PineconeVectorStore
 from llama_index.storage.storage_context import StorageContext
 from langchain.chat_models import ChatOpenAI
 
 load_dotenv()
+# PineconeReader = download_loader("PineconeReader")
+# reader = PineconeReader(
+#     api_key=os.getenv("PINECONE_API_KEY"),
+#     environment="us-west4-gcp"
+# )
+# docs_from_pinecone = reader.load_data(index_name="nietzsche")
 
 urls = [
     "https://www.projekt-gutenberg.org/nietzsch/wanderer/wanderer.html",
     "https://www.projekt-gutenberg.org/nietzsch/wanderer/wande002.html",
     "https://www.projekt-gutenberg.org/nietzsch/wanderer/wande003.html",
-    "https://www.projekt-gutenberg.org/nietzsch/wanderer/wande004.html"
+    "https://www.projekt-gutenberg.org/nietzsch/wanderer/wande004.html",
 ]
 
-def create_pages(urls):
-    
-    pages = []
-    for url in urls:
-        pagename = url.split("/")[-1]
-        pages.append(pagename)
-    
-    return pages
-
-
 def scrape_book(urls):
-    from pathlib import Path
-    result = []
-    
+
     for url in urls:
-
+        result = []
         req = requests.get(url)
-        soup = BeautifulSoup(
-            req.text, 
-            "html.parser",
-        )
+        soup = BeautifulSoup(req.text, "html.parser")
 
-        # keep only the heading tags and <p> tags
+        # keep only the heading tags up to h3, and p tags
         text = soup.find_all(["h1", "h2", "h3", "p"])
 
-        # remove the tags and keep only the text
+        # remove the tags and keep the inner text
         text = [t.text for t in text]
 
         for i in text:
@@ -65,9 +59,15 @@ def scrape_book(urls):
 
         with open(book_path / f"{pagename}.txt", "w") as f:
             f.write("\n".join(result))
-    
-    return result
 
+def create_pages(urls):
+
+    pages = []
+    for url in urls:
+        pagename = url.split("/")[-1]
+        pages.append(pagename)
+
+    return pages
 
 def build_docs(pages):
     docs = {}
@@ -85,55 +85,64 @@ def build_context(model_name):
 
 def build_index(pages, docs):
 
+    page_indices = {}
     pinecone.init(
         api_key=os.getenv("PINECONE_API_KEY"),
         environment="us-west4-gcp"
     )
-    pinecone.create_index("nietzsche", dimension=1536, metric="cosine")
+
+    # create a Pinecone index if you don't have one
+    # https://openai.com/blog/new-and-improved-embedding-model (12288 -> 1536 dimensions)
+    # pinecone.create_index("nietzsche", dimension=1536, metric="cosine")
+    
     pinecone_index = pinecone.Index("nietzsche")
+
+    # pinecone_index.upsert("nietzsche_wandere", [1,2,3])
+    # pinecone_index.describe_index_stats()
+    # pinecone_index.delete_index()
 
     service_context = build_context("gpt-3.5-turbo")
 
-    pages_indexes = {}
     for page in pages:
-        metadata_filters = {"page": page}
+        
         vector_store = PineconeVectorStore(
             pinecone_index=pinecone_index,
-            metadata_filters=metadata_filters
+            metadata_filters={"page": page}
         )
         storage_context = StorageContext.from_defaults(vector_store=vector_store)
-        pages_indexes[page] = GPTVectorStoreIndex.from_documents(
+        page_indices[page] = GPTVectorStoreIndex.from_documents(
             docs[page], storage_context=storage_context, service_context=service_context
         )
-        pages_indexes[page].index_struct.index_id = page
+        page_indices[page].index_struct.index_id = page
 
     print("Indexing complete.")
-    return pages_indexes
-
-def create_query_engine(chapter="wande002.html"):
-    
-    PROMPT_TEMPLATE = (
-        "Here are the context information:"
-        " \n----------------------------------\n"
-        "{context_str}"
-        " \n----------------------------------\n"
-        "Answer the following question in the original german text, then provide an english translation and explanation in as instructive and educational way as possible: {query_str}\n"
-    )
-    QA_PROMPT = QuestionAnswerPrompt(PROMPT_TEMPLATE)
-
-    query_engine = indices[chapter].as_query_engine(
-        text_qa_template=QA_PROMPT
-    )
-    return query_engine
+    return page_indices
 
 if __name__ == "__main__":
+    # uncomment this to download books from project guternberg
     # scrape_book(urls)
+    # assuming books have already been downloaded into your local directory
     pages = create_pages(urls)
-    docs = build_docs(pages) # print(docs.keys())
+    docs = build_docs(pages)
+    # print(docs.keys())
     indices = build_index(pages, docs)
 
-    query_engine = create_query_engine(chapter="wande002.html")
-    response = query_engine.query("what are important things according to Nietzsche?")
+    # response = indices["wande002.html"].as_query_engine().query(
+    #     "What are Nietzsche's view on religion? Answer in the original German text, and provide an English translation for the answer"
+    # )
+
+    PROMPT_TEMPLATE = (
+        "Here are the context information:"
+        "\n-----------------------------\n"
+        "{context_str}"
+        "\n-----------------------------\n"
+        "Answer the following question in the original German text, and provide an english translation and explanation in as instructive and educational way as possible: {query_str} \n"
+    )
+
+    QA_PROMPT = QuestionAnswerPrompt(PROMPT_TEMPLATE)
+    query_engine = indices["wande002.html"].as_query_engine(text_qa_template=QA_PROMPT)
+    response = query_engine.query("What are important things according to Nietzsche?")
 
     print(str(response))
     print(response.get_formatted_sources())
+
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # llm-python
-A set of instructional materials, code samples and Python scripts featuring LLMs (GPT etc) through interfaces like llamaindex, langchain, Chroma, Alpaca etc. Mainly used to store reference code for my LangChain tutorials on YouTube.
+A set of instructional materials, code samples and Python scripts featuring LLMs (GPT etc) through interfaces like llamaindex, langchain, Chroma (Chromadb), Pinecone etc. Mainly used to store reference code for my LangChain tutorials on YouTube. 
 
 <!-- <img src="assets/youtube.png" width="50%" alt="LangChain youtube tutorials" /> -->
 ![LangChain youtube tutorials](assets/llmseries.png)
@@ -12,14 +12,22 @@ Learn LangChain from my YouTube channel:
 - Part 5: [Query any website with LLamaIndex + GPT3 (ft. Chromadb, Trafilatura)](https://youtu.be/6K1lyyzpxtk)
 - Part 6: [Locally-hosted, offline LLM w/LlamaIndex + OPT (open source, instruction-tuning LLM)](https://youtu.be/qAvHs6UNb2k)
 
+### Side Lessons (good supplements to the main series above)
+- [OpenAI tutorial and video walkthrough](https://youtu.be/skw-togjY7Q)
+
 ### Quick Start
 1. Clone this repo
 2. Install requirements: `pip install -r requirements.txt`
 3. Some sample data are provided to you in the `news` foldeer, but you can use your own data by replacing the content (or adding to it) with your own text files.
-4. Create a `.env` file which contains your OpenAI API key. You can get one from [here](https://beta.openai.com/). The `.env` file should look like this:
+4. Create a `.env` file which contains your OpenAI API key. You can get one from [here](https://beta.openai.com/). `HUGGINGFACEHUB_API_TOKEN` and `PINECONE_API_KEY` are optional, but they are used in some of the lessons.
+
+The `.env` file should look like this:
 ```
 OPENAI_API_KEY=your_api_key_here
+
+# optionals (not required for most of the series)
 HUGGINGFACEHUB_API_TOKEN=your_api_token_here
+PINECONE_API_KEY=your_api_key_here
 ```
-HuggingFace is optional but is recommended if you want to use the Inference API and explore those models outside of the OpenAI ecosystem. This is demonstrated in Part 3 of the tutorial series. 
+HuggingFace and Pinecone are optional but is recommended if you want to use the Inference API and explore those models outside of the OpenAI ecosystem. This is demonstrated in Part 3 of the tutorial series. 
 5. Run the examples in any order you want. For example, `python 6_team.py` will run the website Q&A example, which uses GPT-3 to answer questions about a company and the team of people working at Supertype.ai. Watch the corresponding video to follow along each of the examples.
